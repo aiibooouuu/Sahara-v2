721c1fab12d635a477f883896e7eea2a1d203b13
PippyUrkel <andredsouza256@gmail.com>
2025-06-11 22:59:36 +0530

    added an initial BACKEND folder with a basic USER model set up

---
+++ BACKEND/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=30, unique=True)
+    password = models.CharField(max_length=64)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
